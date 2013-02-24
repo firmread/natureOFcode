91c14361784987215fb9660e4ea8969e8de75e2c
Tharit Tothong <litchirhythm@gmail.com>
2013-02-24 14:25:45 -0500

    example version for dorkshop

---
+++ chp2_forces/NOC_2_6_attraction/src/attractor.cpp
@@ -0,0 +1,9 @@
+//
+//  attractor.cpp
+//  NOC_2_6_attraction
+//
+//  Created by Firm Read on 2/23/13.
+//
+//
+
+#include "attractor.h"
---
+++ chp2_forces/NOC_2_6_attraction/src/attractor.h
@@ -0,0 +1,14 @@
+//
+//  attractor.h
+//  NOC_2_6_attraction
+//
+//  Created by Firm Read on 2/23/13.
+//
+//
+
+#ifndef __NOC_2_6_attraction__attractor__
+#define __NOC_2_6_attraction__attractor__
+
+#include <iostream>
+
+#endif /* defined(__NOC_2_6_attraction__attractor__) */
